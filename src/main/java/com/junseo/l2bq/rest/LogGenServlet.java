4dde4a1e58266b0077d4da0e6f753ee80769f218
Junki Kim <jkkim@playearth.co.kr>
2013-05-20 10:09:01 +0900

    Add JAX-RS Depdencies Configuration for RESTful service using JAX-RS Add Empty Class for Log  Gen. REST Service

---
+++ src/main/java/com/junseo/l2bq/rest/LogGenServlet.java
@@ -0,0 +1,14 @@
+package com.junseo.l2bq.rest;
+
+import javax.ws.rs.Path;
+
+@Path("/log")
+public class LogGenServlet
+{
+
+	public LogGenServlet()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
