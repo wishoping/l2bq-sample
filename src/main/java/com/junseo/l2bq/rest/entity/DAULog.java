6084be2923d2c900843e962393fba1daeb76bac0
Junki Kim <jkkim@playearth.co.kr>
2013-05-20 12:28:27 +0900

    app engine dau log 남기는 기능 추가 버전

---
+++ src/main/java/com/junseo/l2bq/rest/entity/DAULog.java
@@ -0,0 +1,37 @@
+package com.junseo.l2bq.rest.entity;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class DAULog
+{
+	private String type;
+
+	private LoginData data;
+	
+	public DAULog()
+	{
+
+	}
+
+	public String getType()
+	{
+		return type;
+	}
+
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+
+	public LoginData getData()
+	{
+		return data;
+	}
+
+	public void setData(LoginData data)
+	{
+		this.data = data;
+	}
+
+}
