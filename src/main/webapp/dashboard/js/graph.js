61a060e959ce07dded374614d5fea80e6e7dc4e0
wsseo <wsseo@playearth.co.kr>
2013-05-23 19:29:47 +0900

    Http Log 추가

--- src/main/webapp/dashboard/js/graph.js
+++ src/main/webapp/dashboard/js/graph.js
@@ -1,8 +1,9 @@
 function showGraph(divName, title, subTitle, x_series, y_series, xTitle, yTitle, seriesName)
 {
+	// http://www.highcharts.com/demo/spline-symbols
   $(divName).highcharts({
     chart: {
-      type: 'line'
+      type: 'spline'
     },
     title: {
       text: title
@@ -18,18 +19,30 @@ function showGraph(divName, title, subTitle, x_series, y_series, xTitle, yTitle,
         text: yTitle
       }
     },
+    credits: {
+        style: {
+            color: '#9fa2a5'
+        }
+    },    
     tooltip: {
-      enabled: false
+        crosshairs: true,
+        shared: true
     },
     plotOptions: {
-      line: {
-        dataLabels: {
-          enabled: true
-        },
-        enableMouseTracking: false
-      }
+        series: {
+            marker: {
+                lineWidth: 1, // The border of each point (defaults to white)
+                radius: 4 // The thickness of each point
+            },
+
+            lineWidth: 3, // The thickness of the line between points
+            shadow: false
+        }
     },
     series: [{
+        marker: {
+            symbol: 'circle'
+        },
       name: seriesName,
       data: y_series
     }]
@@ -90,3 +103,140 @@ function showNewUserGraph(divName, jsonObject, title, subTitle, xTitle, yTitle, 
 
   showGraph(divName, title, subTitle, x_series, y_series, xTitle, yTitle, seriesName);
 }
+
+
+function loadDashboard() {
+	$.get("http://l2bq-test.appspot.com/rest/query", {"query":"select count(*) as totalUsers from [l2bq_test.applog_signup]"},function(data){
+	    var totalUsers = data.list[0].totalUsers;
+	    $("#totalUsers").text(totalUsers);
+	
+	    $.get("http://l2bq-test.appspot.com/rest/query", {"query":"select STRFTIME_UTC_USEC(time*1000, \"%Y-%m-%d\") as f_time, count(*) as loginCount from [l2bq_test.applog_login] GROUP BY f_time ORDER BY f_time"},function(data){
+	      showActiveUserGraph('#dauChart', data, 'DAU', '', 'Time', '# of User Login/hour', 'KaizinRumble');
+	
+	    },'jsonp');
+	
+	    $.get("http://l2bq-test.appspot.com/rest/query", {"query":"select STRFTIME_UTC_USEC(time*1000, \"%Y-%m-%d\") as f_time, count(*) as signupCount from [l2bq_test.applog_signup] GROUP BY f_time ORDER BY f_time"},function(data){
+	      showNewUserGraph('#signupChart', data, 'New Users', '', 'Time', '# of New Users', 'KaizinRumble');
+	    },'jsonp');
+	
+	    $.get("http://l2bq-test.appspot.com/rest/query", {"query":"select visitCount, count(visitCount) as userCount from (select userId, count(userId) as visitCount from (SELECT userId, STRFTIME_UTC_USEC(time*1000, \"%Y-%m-%d\") as day, count(userId) as loginCount FROM [l2bq_test.applog_login] where (time*1000) >= TIMESTAMP_TO_USEC(DATE_ADD(TIMESTAMP(CURRENT_DATE()), -1, \"MONTH\")) group by userId, day order by userId, day) group by userId) group by visitCount order by visitCount desc"},function(data){
+	      showRetentionGraph('#retentionChart', data, 'Retention %', '', 'Day', '% Day 0', 'KaizinRumble', 30, totalUsers);
+	    },'jsonp');
+	
+	    // retention_1
+	    $.get("http://l2bq-test.appspot.com/rest/query", {"query":"select sum(userCount) as userCount from (select visitCount, count(visitCount) as userCount from (select userId, count(userId) as visitCount from (SELECT userId, STRFTIME_UTC_USEC(time*1000, \"%Y-%m-%d\") as day, count(userId) as loginCount FROM [l2bq_test.applog_login] where (time*1000) >= TIMESTAMP_TO_USEC(DATE_ADD(TIMESTAMP('2013-06-12'), -1, \"DAY\")) group by userId, day order by userId, day) group by userId) group by visitCount)"},function(data){
+	      $("#retention_1").text(Math.round (data.list[0].userCount / totalUsers * 10000 ) / 100 + "%");
+	    },'jsonp');
+	
+	    // retention_2
+	    $.get("http://l2bq-test.appspot.com/rest/query", {"query":"select sum(userCount) as userCount from (select visitCount, count(visitCount) as userCount from (select userId, count(userId) as visitCount from (SELECT userId, STRFTIME_UTC_USEC(time*1000, \"%Y-%m-%d\") as day, count(userId) as loginCount FROM [l2bq_test.applog_login] where (time*1000) >= TIMESTAMP_TO_USEC(DATE_ADD(TIMESTAMP('2013-06-12'), -2, \"DAY\")) group by userId, day order by userId, day) group by userId) group by visitCount)"},function(data){
+	      $("#retention_2").text(Math.round (data.list[0].userCount / totalUsers * 10000 ) / 100 + "%");
+	    },'jsonp');
+	
+	    // retention_3
+	    $.get("http://l2bq-test.appspot.com/rest/query", {"query":"select sum(userCount) as userCount from (select visitCount, count(visitCount) as userCount from (select userId, count(userId) as visitCount from (SELECT userId, STRFTIME_UTC_USEC(time*1000, \"%Y-%m-%d\") as day, count(userId) as loginCount FROM [l2bq_test.applog_login] where (time*1000) >= TIMESTAMP_TO_USEC(DATE_ADD(TIMESTAMP('2013-06-12'), -3, \"DAY\")) group by userId, day order by userId, day) group by userId) group by visitCount)"},function(data){
+	      $("#retention_3").text(Math.round (data.list[0].userCount / totalUsers * 10000 ) / 100 + "%");
+	    },'jsonp');
+	
+	    // retention_7
+	    $.get("http://l2bq-test.appspot.com/rest/query", {"query":"select sum(userCount) as userCount from (select visitCount, count(visitCount) as userCount from (select userId, count(userId) as visitCount from (SELECT userId, STRFTIME_UTC_USEC(time*1000, \"%Y-%m-%d\") as day, count(userId) as loginCount FROM [l2bq_test.applog_login] where (time*1000) >= TIMESTAMP_TO_USEC(DATE_ADD(TIMESTAMP('2013-06-12'), -7, \"DAY\")) group by userId, day order by userId, day) group by userId) group by visitCount)"},function(data){
+	      $("#retention_7").text(Math.round (data.list[0].userCount / totalUsers * 10000 ) / 100 + "%");
+	    },'jsonp');
+	
+	    // retention_14
+	    $.get("http://l2bq-test.appspot.com/rest/query", {"query":"select sum(userCount) as userCount from (select visitCount, count(visitCount) as userCount from (select userId, count(userId) as visitCount from (SELECT userId, STRFTIME_UTC_USEC(time*1000, \"%Y-%m-%d\") as day, count(userId) as loginCount FROM [l2bq_test.applog_login] where (time*1000) >= TIMESTAMP_TO_USEC(DATE_ADD(TIMESTAMP('2013-06-12'), -14, \"DAY\")) group by userId, day order by userId, day) group by userId) group by visitCount)"},function(data){
+	      $("#retention_14").text(Math.round (data.list[0].userCount / totalUsers * 10000 ) / 100 + "%");
+	    },'jsonp');
+	
+	    // retention_21
+	    $.get("http://l2bq-test.appspot.com/rest/query", {"query":"select sum(userCount) as userCount from (select visitCount, count(visitCount) as userCount from (select userId, count(userId) as visitCount from (SELECT userId, STRFTIME_UTC_USEC(time*1000, \"%Y-%m-%d\") as day, count(userId) as loginCount FROM [l2bq_test.applog_login] where (time*1000) >= TIMESTAMP_TO_USEC(DATE_ADD(TIMESTAMP('2013-06-12'), -21, \"DAY\")) group by userId, day order by userId, day) group by userId) group by visitCount)"},function(data){
+	      $("#retention_21").text(Math.round (data.list[0].userCount / totalUsers * 10000 ) / 100 + "%");
+	    },'jsonp');
+	
+	    // retention_1mon
+	    $.get("http://l2bq-test.appspot.com/rest/query", {"query":"select sum(userCount) as userCount from (select visitCount, count(visitCount) as userCount from (select userId, count(userId) as visitCount from (SELECT userId, STRFTIME_UTC_USEC(time*1000, \"%Y-%m-%d\") as day, count(userId) as loginCount FROM [l2bq_test.applog_login] where (time*1000) >= TIMESTAMP_TO_USEC(DATE_ADD(TIMESTAMP('2013-06-12'), -1, \"MONTH\")) group by userId, day order by userId, day) group by userId) group by visitCount)"},function(data){
+	      $("#retention_1mon").text(Math.round (data.list[0].userCount / totalUsers * 10000 ) / 100 + "%");
+	    },'jsonp');
+	  },'jsonp');
+}
+
+function loadHttpLogs() {
+	// Total HTTP Logs Count
+	$.get("http://l2bq-test.appspot.com/rest/query", {"query":"select count(*) as totalHttpLogs from [l2bq_test.http_access_log]"},function(data){
+	    var totalHttpLogs = data.list[0].totalHttpLogs;
+	    $("#totalHttpLogs").text(totalHttpLogs);
+	},'jsonp');	
+	
+	
+	$.get("http://l2bq-test.appspot.com/rest/query", {"query":"select level, count(*) as levelCount from [l2bq_test.http_access_log] group by level"},function(data){
+		for (var i in data.list) {
+			var level = data.list[i].level;
+			if (level == "Info") {
+				$("#totalHttpLogsInfo").text(data.list[i].levelCount);
+			}
+			else if (level == "Warning") {
+				$("#totalHttpLogsWarning").text(data.list[i].levelCount);
+			}
+			else if (level == "Critical") {
+				$("#totalHttpLogsCritical").text(data.list[i].levelCount);
+			}
+			else if (level == "Debug") {
+				$("#totalHttpLogsDebug").text(data.list[i].levelCount);
+			}
+			else {
+				$("#totalHttpLogsUndefined").text(data.list[i].levelCount);
+			}
+		}
+	},'jsonp');	
+	
+	
+	// 로그 정보들
+	var limit = 5;
+	
+	// Critical
+	$.get("http://l2bq-test.appspot.com/rest/query", {"query":"select method, timestamp, resource, ip, versionId from [l2bq_test.http_access_log] where level = 'Critical' order by timestamp desc limit " + limit},function(data){
+		var resultHtml = "";
+		for (var i in data.list) {
+			var date = new Date(data.list[i].timestamp / 1000);
+			var row = "<tr><td>" + date + "</td><td>" + data.list[i].ip + "</td><td>" + data.list[i].method + "</td><td>" + data.list[i].resource.substring(0,100) + "</td><td>" + data.list[i].versionId + "</td></tr>";
+			resultHtml += row;
+		}
+		$("#tableCriticalLogs").html(resultHtml);
+	},'jsonp');	
+
+	// Warning
+	$.get("http://l2bq-test.appspot.com/rest/query", {"query":"select method, timestamp, resource, ip, versionId from [l2bq_test.http_access_log] where level = 'Warning' order by timestamp desc limit " + limit},function(data){
+		var resultHtml = "";
+		for (var i in data.list) {
+			var date = new Date(data.list[i].timestamp / 1000);
+			var row = "<tr><td>" + date + "</td><td>" + data.list[i].ip + "</td><td>" + data.list[i].method + "</td><td>" + data.list[i].resource.substring(0,100) + "</td><td>" + data.list[i].versionId + "</td></tr>";
+			resultHtml += row;
+		}
+		$("#tableWarningLogs").html(resultHtml);
+	},'jsonp');	
+
+	// Info
+	$.get("http://l2bq-test.appspot.com/rest/query", {"query":"select method, timestamp, resource, ip, versionId from [l2bq_test.http_access_log] where level = 'Info' order by timestamp desc limit " + limit},function(data){
+		var resultHtml = "";
+		for (var i in data.list) {
+			var date = new Date(data.list[i].timestamp / 1000);
+			var row = "<tr><td>" + date + "</td><td>" + data.list[i].ip + "</td><td>" + data.list[i].method + "</td><td>" + data.list[i].resource.substring(0,100) + "</td><td>" + data.list[i].versionId + "</td></tr>";
+			resultHtml += row;
+		}
+		$("#tableInfoLogs").html(resultHtml);
+	},'jsonp');	
+
+	/*
+	// Debug
+	$.get("http://l2bq-test.appspot.com/rest/query", {"query":"select method, timestamp, resource, ip, versionId from [l2bq_test.http_access_log] where level = 'Debug' order by timestamp desc limit " + limit},function(data){
+		var resultHtml = "";
+		for (var i in data.list) {
+			var date = new Date(data.list[i].timestamp / 1000);
+			var row = "<tr><td>" + date + "</td><td>" + data.list[i].ip + "</td><td>" + data.list[i].method + "</td><td>" + data.list[i].resource.substring(0,100) + "</td><td>" + data.list[i].versionId + "</td></tr>";
+			resultHtml += row;
+		}
+		$("#tableDebugLogs").html(resultHtml);
+	},'jsonp');
+	*/	
+}
+
